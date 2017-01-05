7d057eab9fc9fde6ba81e2056953347fb740a90b
INVESTCLOUD\gzhou <gzhou@investcloud.com>
2017-01-04 19:57:08 -0500

    4th

--- ServerLoadsTest/ServerLoadsControllerTests.cs
+++ ServerLoadsTest/ServerLoadsControllerTests.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 namespace ServerLoads.Test
 {
     [TestClass]
-    public class ServerLoadsControllerTests  //I try to change from TestServerLoads 3rd time
+    public class ServerLoadsControllerTests  //I try to change from TestServerLoads 4th time
     {
         ServerLoadsController _controller;
         [ClassInitialize]
