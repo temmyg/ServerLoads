bbdbee7a433a655fd8a3e9a55adf6b7aa438307d
INVESTCLOUD\gzhou <gzhou@investcloud.com>
2017-01-14 11:23:10 -0500

    no message

--- ServerLoadsTest/ServerLoadsControllerTests.cs
+++ ServerLoadsTest/ServerLoadsControllerTests.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 namespace ServerLoads.Test
 {
     [TestClass]
-    public class ServerLoadsControllerTests  //From 10thBranch 1st time
+    public class ServerLoadsControllerTests  //From BthBranch 1st time
     {
         ServerLoadsController _controller;
         [ClassInitialize]
